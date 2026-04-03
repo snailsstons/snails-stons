7314bd80d2e1e1553996c50f3ac0b1d0f185f484
snailsstons <mrdrag.666@gmail.com>
2026-04-04 00:18:42 +0200

    Update heft0.js

--- data/heft0.js
+++ data/heft0.js
@@ -8,12 +8,12 @@ hefte["0"] = {
   "coverStory": "Ehemalige YouTube Videos",
   "seiten": [
     {
-      "nummer": 2,
+      "nummer": 1,
       "titel": "",
       "akt": "",
       "panels": [
         {
-          "bild": "0_2_P1.mp4",
+          "bild": "0_1_P1.mp4",
           "format": "16:9",
           "wide": true,
           "tall": false,
@@ -23,7 +23,7 @@ hefte["0"] = {
           "caption": ""
         },
         {
-          "bild": "0_2_P2.jpg",
+          "bild": "0_1_P2.jpg",
           "format": "1:1",
           "wide": false,
           "tall": false,
@@ -35,12 +35,12 @@ hefte["0"] = {
       ]
     },
     {
-      "nummer": 3,
+      "nummer": 2,
       "titel": "",
       "akt": "",
       "panels": [
         {
-          "bild": "0_3_P1.jpg",
+          "bild": "0_2_P1.jpg",
           "format": "16:9",
           "wide": true,
           "tall": false,
@@ -50,7 +50,7 @@ hefte["0"] = {
           "caption": ""
         },
         {
-          "bild": "0_3_P2.jpg",
+          "bild": "0_2_P2.jpg",
           "format": "1:1",
           "wide": false,
           "tall": false,
